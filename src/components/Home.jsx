bbbe331f243ad3848ed4d5f4dde6909aad93ac3a
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2024-08-31 06:07:13 +0700

    Update Router Dom List Product

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ListProduct from "./ListProduct";
+
 function Home() {
   return (
     <>
+      {/* router dom */}
+
       {/* Navbar */}
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
@@ -40,7 +46,7 @@ function Home() {
             <div className="mt-8 flex flex-wrap justify-center gap-4">
               <a
                 className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                href="#"
+                href="products"
               >
                 Lihat Product
               </a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home"; // Halaman utama
+import ListProduct from "./components/ListProduct"; // Halaman produk
 import "./index.css";
-import Home from "./components/Home.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
-    {/* <App /> */}
+    <Router>
+      <Routes>
+        {/* Route untuk halaman Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* Route untuk halaman ListProduct */}
+        <Route path="/products" element={<ListProduct />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
