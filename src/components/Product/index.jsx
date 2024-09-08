87a9e8e50024e635b45ec70819da002aa856aec3
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2024-09-09 03:03:03 +0700

    Update

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -5,342 +5,44 @@ const Product = () => {
   return (
     <section>
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <header>
+        <header className="text-center">
           <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
             Product Collection
           </h2>
 
-          <p className="mt-4 max-w-md text-gray-500">
-            "Pilihan Kerudung Terbaik untuk Penampilan Sempurna! Tersedia Banyak
-            Model, Siap Kirim!"
+          <p className="mx-auto mt-4 max-w-md text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            praesentium cumque iure dicta incidunt est ipsam, officia dolor
+            fugit natus?
           </p>
         </header>
 
-        <div className="mt-8 sm:flex sm:items-center sm:justify-between">
-          <div className="block sm:hidden">
-            <button className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-              <span className="text-sm font-medium"> Filters & Sorting </span>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="size-4 rtl:rotate-180"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </button>
-          </div>
-
-          <div className="hidden sm:flex sm:gap-4">
-            <div className="relative">
-              <details className="group [&_summary::-webkit-details-marker]:hidden">
-                <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-                  <span className="text-sm font-medium"> Availability </span>
-
-                  <span className="transition group-open:-rotate-180">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="size-4"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                      />
-                    </svg>
-                  </span>
-                </summary>
-
-                <div className="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
-                  <div className="w-96 rounded border border-gray-200 bg-white">
-                    <header className="flex items-center justify-between p-4">
-                      <span className="text-sm text-gray-700">
-                        {" "}
-                        0 Selected{" "}
+        <ul className="mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4 justify-center items-center">
+          {/* List of products */}
+          {Array(4)
+            .fill()
+            .map((_, index) => (
+              <li key={index}>
+                <a href="#" className="group block overflow-hidden">
+                  <img
+                    src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                    alt="Product"
+                    className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                  />
+                  <div className="relative bg-white pt-3">
+                    <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                      Basic Tee
+                    </h3>
+                    <p className="mt-2">
+                      <span className="sr-only"> Regular Price </span>
+                      <span className="tracking-wider text-gray-900">
+                        £24.00 GBP
                       </span>
-
-                      <button
-                        type="button"
-                        className="text-sm text-gray-900 underline underline-offset-4"
-                      >
-                        Reset
-                      </button>
-                    </header>
-
-                    <ul className="space-y-1 border-t border-gray-200 p-4">
-                      <li>
-                        <label
-                          for="FilterInStock"
-                          className="inline-flex items-center gap-2"
-                        >
-                          <input
-                            type="checkbox"
-                            id="FilterInStock"
-                            className="size-5 rounded border-gray-300"
-                          />
-
-                          <span className="text-sm font-medium text-gray-700">
-                            {" "}
-                            In Stock (5+){" "}
-                          </span>
-                        </label>
-                      </li>
-
-                      <li>
-                        <label
-                          for="FilterPreOrder"
-                          className="inline-flex items-center gap-2"
-                        >
-                          <input
-                            type="checkbox"
-                            id="FilterPreOrder"
-                            className="size-5 rounded border-gray-300"
-                          />
-
-                          <span className="text-sm font-medium text-gray-700">
-                            {" "}
-                            Pre Order (3+){" "}
-                          </span>
-                        </label>
-                      </li>
-
-                      <li>
-                        <label
-                          for="FilterOutOfStock"
-                          className="inline-flex items-center gap-2"
-                        >
-                          <input
-                            type="checkbox"
-                            id="FilterOutOfStock"
-                            className="size-5 rounded border-gray-300"
-                          />
-
-                          <span className="text-sm font-medium text-gray-700">
-                            {" "}
-                            Out of Stock (10+){" "}
-                          </span>
-                        </label>
-                      </li>
-                    </ul>
+                    </p>
                   </div>
-                </div>
-              </details>
-            </div>
-
-            <div className="relative">
-              <details className="group [&_summary::-webkit-details-marker]:hidden">
-                <summary className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600">
-                  <span className="text-sm font-medium"> Price </span>
-
-                  <span className="transition group-open:-rotate-180">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      className="size-4"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                      />
-                    </svg>
-                  </span>
-                </summary>
-
-                <div className="z-50 group-open:absolute group-open:top-auto group-open:mt-2 ltr:group-open:start-0">
-                  <div className="w-96 rounded border border-gray-200 bg-white">
-                    <header className="flex items-center justify-between p-4">
-                      <span className="text-sm text-gray-700">
-                        {" "}
-                        The highest price is $600{" "}
-                      </span>
-
-                      <button
-                        type="button"
-                        className="text-sm text-gray-900 underline underline-offset-4"
-                      >
-                        Reset
-                      </button>
-                    </header>
-
-                    <div className="border-t border-gray-200 p-4">
-                      <div className="flex justify-between gap-4">
-                        <label
-                          for="FilterPriceFrom"
-                          className="flex items-center gap-2"
-                        >
-                          <span className="text-sm text-gray-600">$</span>
-
-                          <input
-                            type="number"
-                            id="FilterPriceFrom"
-                            placeholder="From"
-                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                          />
-                        </label>
-
-                        <label
-                          for="FilterPriceTo"
-                          className="flex items-center gap-2"
-                        >
-                          <span className="text-sm text-gray-600">$</span>
-
-                          <input
-                            type="number"
-                            id="FilterPriceTo"
-                            placeholder="To"
-                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                          />
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </details>
-            </div>
-          </div>
-
-          <div className="hidden sm:block">
-            <label for="SortBy" className="sr-only">
-              SortBy
-            </label>
-
-            <select
-              id="SortBy"
-              className="h-10 rounded border-gray-300 text-sm"
-            >
-              <option>Sort By</option>
-              <option value="Title, DESC">Title, DESC</option>
-              <option value="Title, ASC">Title, ASC</option>
-              <option value="Price, DESC">Price, DESC</option>
-              <option value="Price, ASC">Price, ASC</option>
-            </select>
-          </div>
-        </div>
-
-        {/* group colection */}
-        <ul className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-          <li className="group block overflow-hidden">
-            <a
-              href="https://wa.me/6285707185783?text=Halo%20Admin,%20saya%20ingin%20bertanya%20tentang%20produk%20'Kerudung%20Plasmina'%20yang%20berharga%20£24.00%20GBP."
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Basic Tee"
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  Basic Tee
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only">Regular Price</span>
-                  <span className="tracking-wider text-gray-900">
-                    £24.00 GBP
-                  </span>
-                </p>
-              </div>
-            </a>
-          </li>
-
-          <li className="group block overflow-hidden">
-            <a
-              href="https://wa.me/6285707185783?text=Halo%20Admin,%20saya%20ingin%20bertanya%20tentang%20produk%20'Kerudung%20Plasmina'%20yang%20berharga%20£24.00%20GBP."
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Basic Tee"
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  Basic Tee
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only">Regular Price</span>
-                  <span className="tracking-wider text-gray-900">
-                    £24.00 GBP
-                  </span>
-                </p>
-              </div>
-            </a>
-          </li>
-
-          <li className="group block overflow-hidden">
-            <a
-              href="https://wa.me/6285707185783?text=Halo%20Admin,%20saya%20ingin%20bertanya%20tentang%20produk%20'Kerudung%20Plasmina'%20yang%20berharga%20£24.00%20GBP."
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Basic Tee"
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  Basic Tee
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only">Regular Price</span>
-                  <span className="tracking-wider text-gray-900">
-                    £24.00 GBP
-                  </span>
-                </p>
-              </div>
-            </a>
-          </li>
-
-          <li className="group block overflow-hidden">
-            <a
-              href="https://wa.me/6285707185783?text=Halo%20Admin,%20saya%20ingin%20bertanya%20tentang%20produk%20'Kerudung%20Plasmina'%20yang%20berharga%20£24.00%20GBP."
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Basic Tee"
-                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-              />
-
-              <div className="relative bg-white pt-3">
-                <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-                  Basic Tee
-                </h3>
-
-                <p className="mt-2">
-                  <span className="sr-only">Regular Price</span>
-                  <span className="tracking-wider text-gray-900">
-                    £24.00 GBP
-                  </span>
-                </p>
-              </div>
-            </a>
-          </li>
+                </a>
+              </li>
+            ))}
         </ul>
       </div>
     </section>
